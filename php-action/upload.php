26405f5390aa3552e044fd4d6873aaeaee56cb2f
Antonio Jack <gamertense@live.com>
2017-09-20 20:52:25 +0700

    Increase image upload file size

--- php-action/upload.php
+++ php-action/upload.php
@@ -5,7 +5,7 @@ if (isset($_FILES["file"]["type"])) {
     $temporary = explode(".", $_FILES["file"]["name"]);
     $file_extension = end($temporary);
     if ((($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/jpeg")
-        ) && ($_FILES["file"]["size"] < 1500000) //Approx. 1500kb files can be uploaded.
+        ) && ($_FILES["file"]["size"] < 5000000) //Approx. 5 MB files can be uploaded.
         && in_array($file_extension, $validextensions)
     ) {
         if ($_FILES["file"]["error"] > 0) {
