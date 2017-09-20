dda5a511e4e2ecc2ca84cb35914bded9bdbd0e6c
Antonio Jack <gamertense@live.com>
2017-09-20 20:24:43 +0700

    Change them and fix image height on homepage.

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Thai Food Delivery</title>
+    <link rel="stylesheet" type="text/css" href="css/index-style.css">
+
 </head>
 <body>
 <?php
@@ -10,7 +12,7 @@ require_once('menu.php');
 ?>
 
 <div class="container" style="width:60%;">
-    <h2 align="center">Select food</h2>
+    <h2 align="center">Select food</h2><br>
     <?php
     if (!isset($_GET['input-product']))
         $query = "SELECT * FROM foods ORDER BY food_id";
@@ -20,31 +22,44 @@ require_once('menu.php');
     }
 
     $result = mysqli_query($connect, $query); ?>
-    <form method="post" id="foodsForm">
-        <?php
-        if (mysqli_num_rows($result) > 0):
-            while ($row = mysqli_fetch_array($result)):
-                ?>
-                <div class="col-md-6">
-                    <div style="border: 1px solid #eaeaec; margin: -1px 19px 3px -1px; box-shadow: 0 1px 2px rgba(0,0,0,0.05); padding:10px;"
-                         align="center">
-                        <img src="<?php echo $row["image"]; ?>" class="img-responsive">
-                        <h5 class="text-info"><?php echo $row["name"]; ?></h5>
-                        <h5 class="text-danger">฿<?php echo $row["price"]; ?></h5>
-                        <button name="addButton" style="margin-top:5px;" class="btn btn-success"
-                                value="<?php echo $row["food_id"]; ?>"> Add to Cart
-                        </button>
-                        <button name="wishButton" style="margin-top:5px;" class="btn btn-info"
-                                value="<?php echo $row["food_id"]; ?>"> Add to Wishlist
-                        </button>
-                    </div>
-
+    <?php
+    if (mysqli_num_rows($result) > 0):
+        while ($row = mysqli_fetch_array($result)):
+            ?>
+            <form method="post" id="foodsForm">
+                <div class="col-sm-4">
+                    <article class="col-item">
+                        <div class="photo">
+                            <div class="options-cart-round">
+                                <button class="btn btn-default" title="Add to cart" data-toggle="tooltip">
+                                    <span class="fa fa-shopping-cart"></span>
+                                </button>
+                            </div>
+                            <div class="options-wishlist-round">
+                                <button class="btn btn-default" title="Add to wishlist" data-toggle="tooltip">
+                                    <span class="fa fa-heart"></span>
+                                </button>
+                            </div>
+                            <img src="<?php echo $row["image"]; ?>" class="img-responsive"
+                                 alt="Product Image"/>
+                        </div>
+                        <div class="info">
+                            <div class="row">
+                                <div class="price-details col-md-6">
+                                    <!--                                    <p class="details"> Lorem ipsum dolor sit amet, consectetur.. </p>-->
+                                    <h1><?php echo $row["name"]; ?></h1>
+                                    <br>
+                                    <span class="price-new">฿<?php echo $row["price"]; ?></span>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
                 </div>
-                <?php
-            endwhile;
-        endif;
-        ?>
-    </form>
+            </form>
+            <?php
+        endwhile;
+    endif;
+    ?>
 </div>
 </body>
 </html>
@@ -53,6 +68,7 @@ require_once('menu.php');
     var foodID, btnString = 'cart';
 
     $(document).ready(function () {
+        $('[data-toggle="tooltip"]').tooltip();
         initialLoad();
     });
 
@@ -69,10 +85,11 @@ require_once('menu.php');
             // Stop form from submitting normally
             event.preventDefault();
             // Send the data using post
+            var posting;
             if (btnString === 'cart')
-                var posting = $.post("php-action/add-cart.php", {hidden_id: foodID});
+                posting = $.post("php-action/add-cart.php", {hidden_id: foodID});
             else
-                var posting = $.post("php-action/add-wishlist.php", {hidden_id: foodID});
+                posting = $.post("php-action/add-wishlist.php", {hidden_id: foodID});
             // Put the results in a div
             posting.done(function (data) {
                 alert(data);
