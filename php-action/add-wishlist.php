b22ab3983e946c71a6195e66864d683aeca98ad2
Antonio Jack <gamertense@live.com>
2017-10-19 21:39:58 +0700

    Add "Insert wishlist where cu_id = logged in user"

--- php-action/add-wishlist.php
+++ php-action/add-wishlist.php
@@ -1,14 +1,16 @@
 <?php
+session_start();
 include_once '../dbconfig.php';
 
+$cu_id = $_SESSION["cu_id"];
 $id = $_POST['hidden_id'];
-$query = "SELECT * FROM wishlist WHERE food_id= $id";
+$query = "SELECT * FROM wishlist WHERE food_id = $id and cu_id = $cu_id";
 $result = mysqli_query($connect, $query);
 
 if (mysqli_num_rows($result) > 0) {
     echo "already added to wishlist";
 } else {
-    $query = "INSERT INTO wishlist (food_id) VALUES ($id)";
+    $query = "INSERT INTO wishlist (cu_id, food_id) VALUES ($cu_id, $id)";
 
     if ($connect->query($query) === TRUE) {
         echo "success-wishlist";
