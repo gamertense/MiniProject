a7be729b32b57955df0465f40d47cc3ad494666a
Antonio Jack <gamertense@live.com>
2017-11-12 22:18:52 +0700

    Update createSampleSales.php which I'm not going to use it anyway.

--- admin/createSampleSales.php
+++ admin/createSampleSales.php
@@ -9,10 +9,11 @@ for ($i = 1; $i <= 10; $i++) {
 
     $date = '2017-' . $m . '-22';
 
-    $r_times = rand(1, 3);
+    $r_times = rand(1, 5);
     for ($j = 1; $j <= $r_times; $j++) {
-        $r_qty = rand(1, 20);
-        $sql = "INSERT INTO orders (cu_id, food_id, orderDate, quantity) VALUES ('1', '2', '$date', '$r_qty')";
+    	$r_cu = rand(1, 3);
+        $r_qty = rand(1, 3);
+        $sql = "INSERT INTO orders (cu_id, food_id, orderDate, quantity) VALUES ('$r_cu', '2', '$date', '$r_qty')";
 
         if ($connect->query($sql) === TRUE) {
             echo "New record created successfully";
