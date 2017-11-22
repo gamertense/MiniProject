ddfb12d97fbd1f0b852259e50c2844f8a8ef948d
Antonio Jack <gamertense@live.com>
2017-11-22 18:05:15 +0700

    Redesign and pull ingredients from DB.

--- food-info.php
+++ food-info.php
@@ -10,49 +10,67 @@ $query = "SELECT * FROM foods WHERE food_id = $fid";
 $result = mysqli_query($connect, $query);
 $row = $result->fetch_array();
 ?>
-<div class="container" style="padding-top: 30px">
-    <img class="img-circle" src="<?= $row['image'] ?>" height="35%">
-    <center><h2><font color="#DF013A"><B><?= $row['name'] ?><B></font></h2></center>
-    <br>
-    <h3>
-        <CENTER>ADD-ONS (choose up to 1 )</CENTER>
-    </h3>
+<div class="container">
+    <div class="main col-md-2 col-md-offset-3">
+        <img class="img-circle" src="<?= $row['image'] ?>" height="25%">
+        <center><h2><font color="#DF013A"><B><?= $row['name'] ?><B></font></h2></center>
+        <div class="price"><h3> PRICE : ฿<?= $row['price'] ?></h3></td></div>
+        <div class="ingredient">
+            <h4>Ingredients</h4>
+            <ul>
+                <?php
+                $ing = $row['ingredients'];
+                $pieces = explode("|", $ing);
+                for ($i = 0; $i < count($pieces); $i++) { ?>
+                    <li><?= $pieces[$i] ?></li>
+                <?php } ?>
 
-    <div class="checkbox">
-        <label><input type="checkbox" value="">Rice (ข้าวสวย)</label>
+            </ul>
+        </div>
+        <form method="post" id="foodsForm">
+            <div class="actionBtn">
+                <button name="addButton" class="btn btn-success" title="Add to cart"
+                        data-toggle="tooltip" value="<?php echo $row["food_id"]; ?>">
+                    <span class="fa fa-shopping-cart"></span> Add to cart
+                </button>
+                <button name="wishButton" class="btn btn-danger" title="Add to wishlist"
+                        data-toggle="tooltip" value="<?php echo $row["food_id"]; ?>">
+                    <span class="fa fa-heart"></span> Add to wishlist
+                </button>
+            </div>
+        </form>
     </div>
-    <div class="checkbox">
-        <label><input type="checkbox" value="">Boiled rice (ข้าวต้ม)</label>
-    </div>
-    <br><br>
-    <tr>
-        <h2><font color="#3B170B">
-                PRICE : 45 ฿</font></h2></td>
-    </tr>
 </div>
 </body>
 </html>
 
+<script src="vendor/js/food.js"></script>
+
 <style>
+    .main {
+        box-shadow: 0 0 15px #888888;
+        width: 50%;
+    }
+
+    .actionBtn {
+        margin: auto;
+        padding-bottom: 15px;
+        width: 50%;
+    }
+
+    .price {
+        color: #DF013A;
+        padding: 15px;
+    }
+
+    .ingredient {
+        padding: 15px;
+    }
+
     .img-circle {
+        padding-top: 15px;
         display: block;
         margin: 0 auto;
         border-radius: 50%;
-    }
-
-    .checkbox {
-        font-size: 1.4em;
-        color: #FE2E64;
-        text-align: center;
-    }
-
-    body {
-        background: url("images/plate.jpg") no-repeat center;
-        background-size: cover;
-        -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover;
-        background-attachment: fixed;
-        font-family: 'PT Sans', sans-serif;
     }
 </style>
