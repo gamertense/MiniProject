549f1b44f262eaf324b312ae844126c0f1e77e65
Antonio Jack <gamertense@live.com>
2017-11-22 18:33:34 +0700

    Add ingredients column

--- 1tfd.sql
+++ 1tfd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2017 at 04:12 PM
+-- Generation Time: Nov 22, 2017 at 12:31 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -89,28 +89,29 @@ CREATE TABLE `foods` (
   `name` varchar(255) NOT NULL,
   `price` double(10,2) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `out_stock` int(1) NOT NULL DEFAULT '0' COMMENT '0 = in stock, 1 = out of stock'
+  `out_stock` int(1) NOT NULL DEFAULT '0' COMMENT '0 = in stock, 1 = out of stock',
+  `ingredients` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `foods`
 --
 
-INSERT INTO `foods` (`food_id`, `category_id`, `name`, `price`, `image`, `out_stock`) VALUES
-(2, 1, 'Som Tam', 30.00, 'food-images/som-tam.jpg', 1),
-(3, 2, 'Spicy Noodle Salad', 40.00, 'food-images/maxresdefault.jpg', 0),
-(4, 1, 'Thai Basil Chicken', 45.00, 'food-images/thai-chicken-basil-recipe.jpg', 0),
-(5, 1, 'Tom Yum Koong', 50.00, 'food-images/3161c4_b4d35f6bdc724266969c13020d477eac.jpg', 0),
-(7, 1, 'Porridge With Fish', 35.00, 'food-images/Image.jpg', 0),
-(8, 1, 'Thai Spicy Grilled Pork Salad', 50.00, 'food-images/19510377-Moo-Nam-Tok-Thai-Spicy-Grilled-Pork-Salad-signature-dish-in-thailand-Stock-Photo.jpg', 0),
-(9, 2, 'Special Fish Cakes', 55.00, 'food-images/Thaifishcakes.jpg', 0),
-(10, 1, 'Casseroled Shrimps With Glass Noodles', 50.00, 'food-images/maxresdefault (1).jpg', 0),
-(11, 1, 'Pork Stir-Fried With Garlic And Peppercorns', 40.00, 'food-images/thai-fried-pork-with-garlic-and-pepper-recipe.jpg', 0),
-(12, 2, 'Fried Chicken', 35.00, 'food-images/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg', 0),
-(13, 1, 'Green Chicken Curry', 40.00, 'food-images/global-recipes-01_cropped-green_curry2.jpg', 0),
-(15, 2, 'Crispy Wonton', 30.00, 'food-images/crispy-pork-wontons.jpg', 0),
-(17, 1, 'Honey Rost Duck', 70.00, 'food-images/HoneyRoastedDuck.jpg', 0),
-(18, 1, 'Quick-Fried Water Spinach Seasoned With Chili And Soy Sauce', 45.00, 'food-images/maxresdefault (2).jpg', 0);
+INSERT INTO `foods` (`food_id`, `category_id`, `name`, `price`, `image`, `out_stock`, `ingredients`) VALUES
+(2, 1, 'Som Tam', 30.00, 'food-images/som-tam.jpg', 1, '2 cups shredded green papaya|1/2 cup shredded carrot|1/2 cup sting bean (cut into 1\" long)'),
+(3, 2, 'Spicy Noodle Salad', 40.00, 'food-images/maxresdefault.jpg', 0, ''),
+(4, 1, 'Thai Basil Chicken', 45.00, 'food-images/thai-chicken-basil-recipe.jpg', 0, ''),
+(5, 1, 'Tom Yum Koong', 50.00, 'food-images/3161c4_b4d35f6bdc724266969c13020d477eac.jpg', 0, ''),
+(7, 1, 'Porridge With Fish', 35.00, 'food-images/Image.jpg', 0, ''),
+(8, 1, 'Thai Spicy Grilled Pork Salad', 50.00, 'food-images/19510377-Moo-Nam-Tok-Thai-Spicy-Grilled-Pork-Salad-signature-dish-in-thailand-Stock-Photo.jpg', 0, ''),
+(9, 2, 'Special Fish Cakes', 55.00, 'food-images/Thaifishcakes.jpg', 0, ''),
+(10, 1, 'Casseroled Shrimps With Glass Noodles', 50.00, 'food-images/maxresdefault (1).jpg', 0, ''),
+(11, 1, 'Pork Stir-Fried With Garlic And Peppercorns', 40.00, 'food-images/thai-fried-pork-with-garlic-and-pepper-recipe.jpg', 0, ''),
+(12, 2, 'Fried Chicken', 35.00, 'food-images/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg', 0, ''),
+(13, 1, 'Green Chicken Curry', 40.00, 'food-images/global-recipes-01_cropped-green_curry2.jpg', 0, ''),
+(15, 2, 'Crispy Wonton', 30.00, 'food-images/crispy-pork-wontons.jpg', 0, ''),
+(17, 1, 'Honey Rost Duck', 70.00, 'food-images/HoneyRoastedDuck.jpg', 0, ''),
+(18, 1, 'Quick-Fried Water Spinach Seasoned With Chili And Soy Sauce', 45.00, 'food-images/maxresdefault (2).jpg', 0, '');
 
 -- --------------------------------------------------------
 
